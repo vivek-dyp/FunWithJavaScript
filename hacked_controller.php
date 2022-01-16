aec4173a244532a0cfc088079521b2c017084330
vivek-dyp <63222924+vivek-dyp@users.noreply.github.com>
2022-01-16 15:33:42 +0530

    Add files via upload

---
+++ hacked_controller.php
@@ -0,0 +1,4 @@
+<?php
+    error_reporting(0);
+    echo "<h1>HACKED</h1>";
+?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<style>
+body {
+  background: #f06d06;
+  font-size: 280%;
+  padding: 20px;
+}
+</style>
+<body>
+    <div>THIS WEBSITE HAS BEEN HACKED</div>
+</body>
